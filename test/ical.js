249578d2f1ca5d57b7a686ccdba445060558e4f3
Damian Krzeminski <pirxpilot@code42day.com>
2015-05-13 20:18:34 -0400

    implementation and tests

--- test/ical.js
+++ test/ical.js
@@ -1,9 +1,49 @@
-var should = require('should');
-var furkotIcal = require('../');
+var ical = require('../');
+
+var fs = require('fs');
+var path = require('path');
+
+function readFileSync(name) {
+  return fs.readFileSync(path.join(__dirname, name), 'utf8');
+}
+
+/**
+ * Compare files filtering our time stamps
+ */
+function compareLines(actual, expected) {
+  function notStamp(line) {
+    return line.slice(0, 'DTSTAMP'.length) !== 'DTSTAMP';
+  }
+
+  var i;
+
+  actual = actual.split('\r\n').filter(notStamp);
+  expected = expected.split('\r\n').filter(notStamp);
+
+  actual.should.have.length(expected.length);
+
+  for(i = 0; i < actual.length; i += 1) {
+    actual[i].should.eql(expected[i]);
+  }
+}
+
 
 describe('furkot-ical node module', function () {
-  it('must have at least one test', function () {
-    furkotIcal();
-    should.fail('Need to write tests.');
+
+  it('simple trip', function() {
+    var t = require('./fixtures/simple-trip.json'),
+      generated = ical(t),
+      expected = readFileSync('fixtures/simple.ics');
+
+    compareLines(generated, expected);
   });
+
+  it('multi trip', function() {
+    var t = require('./fixtures/multi-trip.json'),
+      generated = ical(t),
+      expected = readFileSync('fixtures/multi.ics');
+
+    compareLines(generated, expected);
+  });
+
 });
